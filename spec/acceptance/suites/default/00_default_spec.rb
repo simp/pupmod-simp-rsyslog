b79109ef0b10558ddaee19f0fd585d4e77a883c8
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-01-13 12:49:58 -0500

    Fixed spec tests for EL7

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -3,6 +3,22 @@ require 'spec_helper_acceptance'
 test_name 'rsyslog class'
 
 describe 'rsyslog class' do
+
+  before(:context) do
+    hosts.each do |host|
+      interfaces = fact_on(host, 'interfaces').strip.split(',')
+      interfaces.delete_if do |x|
+        x =~ /^lo/
+      end
+
+      interfaces.each do |iface|
+        if fact_on(host, "ipaddress_#{iface}").strip.empty?
+          on(host, "ifup #{iface}", :accept_all_exit_codes => true)
+        end
+      end
+    end
+  end
+
   package_name = 'rsyslog'
   if fact('osfamily') == 'RedHat' && fact('operatingsystemmajrelease') == '6'
     package_name = 'rsyslog7'
--- spec/acceptance/suites/default/04_failover_no_tls_spec.rb
+++ spec/acceptance/suites/default/04_failover_no_tls_spec.rb
@@ -3,6 +3,22 @@ require 'spec_helper_acceptance'
 test_name 'client -> 2 server without TLS'
 
 describe 'rsyslog class' do
+
+  before(:context) do
+    hosts.each do |host|
+      interfaces = fact_on(host, 'interfaces').strip.split(',')
+      interfaces.delete_if do |x|
+        x =~ /^lo/
+      end
+
+      interfaces.each do |iface|
+        if fact_on(host, "ipaddress_#{iface}").strip.empty?
+          on(host, "ifup #{iface}", :accept_all_exit_codes => true)
+        end
+      end
+    end
+  end
+
   before(:all) do
     # Ensure that our test doesn't match messages from other tests
     sleep(1)
